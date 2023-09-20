6d4486d4cce83f85188d9aa6b7d841c4a4676aab
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-09-20 21:39:08 +0300

    update: main index file
    TODO:
    - rewrite import components in router (Loadable)
    - create empty pages (errors, home, auth)
    - add claim guard

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// redux
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './redux/store';
-
 import AuthContainer from './guards/auth.container';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <ReduxProvider store={store}>
     <AuthContainer>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </AuthContainer>
-  </ReduxProvider>
+  </ReduxProvider>,
 );
 
 reportWebVitals();
