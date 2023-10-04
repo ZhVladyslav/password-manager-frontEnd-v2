e10984716ece0ac8f53d2bf519e2a706d859fdfb
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-10-04 13:06:51 +0300

    update: role pages

--- src/pages/role/List.page.tsx
+++ src/pages/role/List.page.tsx
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { roleService } from '../../services/role.service';
+import { IRole } from '../../types/role.type';
+import { useNavigate } from 'react-router-dom';
+import { PATH_ROLE } from '../../routes/paths';
 
 export default function RoleListPage() {
-  
+  const navigate = useNavigate();
+
+  const [roleList, setRoleList] = useState<IRole[]>([]);
+
+  useEffect(() => {
+    getRoleList();
+  }, []);
+
+  const getRoleList = async () => {
+    const list = await roleService.getAll();
+    if (!list) return;
+    setRoleList(list);
+  };
+
   return (
     <>
       <div>
-        <span>RoleListPage</span>
+        {roleList.map((item) => (
+          <span key={item.id} onClick={() => navigate(`${PATH_ROLE.VIEW}/${item.id}`)}>
+            {item.name_en}
+          </span>
+        ))}
       </div>
     </>
   );
--- src/pages/role/View.page.tsx
+++ src/pages/role/View.page.tsx
@@ -1,12 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { IRoleAndClaims } from '../../types/role.type';
+import { roleService } from '../../services/role.service';
+import { PATH_ROLE } from '../../routes/paths';
 
 export default function RoleViewPage() {
-  
+  const { id } = useParams();
+
+  const [claimList, setClaimList] = useState<string[]>([]);
+  const [role, setRole] = useState<IRoleAndClaims | null>(null);
+
+  useEffect(() => {
+    getClaimList();
+    getRole();
+  }, []);
+
+  const getClaimList = async () => {
+    const claimList = await roleService.getClaims();
+    if (!claimList) return;
+    setClaimList(claimList);
+  };
+
+  const getRole = async () => {
+    if (!id) return;
+    const roleInDb = await roleService.getById({ id });
+    if (!roleInDb) return;
+    setRole(roleInDb);
+  };
+
+  console.log(role);
+
+  if (!id) return <Navigate to={PATH_ROLE.LIST} />;
+
   return (
     <>
       <div>
-        <span>RoleViewPage</span>
+        {claimList.map((item, i) => (
+          <div key={i}>
+            <span>{item}</span> <br />
+          </div>
+        ))}
       </div>
+      <br />
+      {role && <div>{role.name_en}</div>}
     </>
   );
 }
