a1d9c16000ec225559f1b06d02fab89b1e07a634
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-10-09 01:53:28 +0300

    update: axios config

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,29 @@
 import axiosImport from 'axios';
 import { API } from './config';
+import { userActions } from '../redux/actions/userActions';
+import { sessionActions } from '../redux/actions/sessionActions';
+import { userSession } from '../auth/userSession';
+
+const logout = async () => {
+  userActions.logout();
+  sessionActions.close();
+  userSession.close();
+};
 
 const axios = axiosImport.create({
   baseURL: API,
 });
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response) {
+      if (error.response.status === 401) logout();
+    }
+    return Promise.reject((error.response && error.response.data) || 'Something went wrong');
+  },
+);
 
 export default axios;
