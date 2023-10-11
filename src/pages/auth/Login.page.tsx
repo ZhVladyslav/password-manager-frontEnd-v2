d61012461af7c6773ace6060937a06d4a2e41087
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-10-11 21:05:36 +0300

    add: login page styles

--- src/pages/auth/Login.page.tsx
+++ src/pages/auth/Login.page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { authService } from '../../services/auth.service';
 import { userSession } from '../../auth/userSession';
 import { useNavigate } from 'react-router-dom';
@@ -11,15 +11,12 @@ import Button from '../../components/Button.component';
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const loginInput = useInputText({ reg: /[0-9]/, errorText: 'test' });
+  const loginInput = useInputText(/* { reg: /[0-9]/, errorText: 'test' } */);
   const passwordInput = useInputText();
-
-  const [login, setLogin] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
 
   const submit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const loginRes = await authService.login({ login, password });
+    const loginRes = await authService.login({ login: loginInput.value, password: passwordInput.value });
     if (!loginRes) return;
     if ('token' in loginRes) userSession.create(loginRes.token);
   };
@@ -27,17 +24,24 @@ export default function LoginPage() {
   return (
     <>
       <div className={style.container}>
-        <div className={style.inner_container}>
-          <div className={style.content}>
+        <div className={style.content_container}>
+          <div className={style.content_block}>
             <h1>Welcome</h1>
-            <h2>Log in in your account</h2>
+            <h2>Login in your account</h2>
             <form onSubmit={submit}>
-              <InputText title="Login" name="login" inputHook={loginInput} />
-              <InputText title="Password" name="password" inputHook={passwordInput} />
-              <Button type="submit" title="submit" />
+              <div className={style.inputBlock}>
+                <InputText title="Login" name="login" inputHook={loginInput} />
+                <InputText title="Password" name="password" inputHook={passwordInput} />
+              </div>
+              <div className={style.formButton}>
+                <Button type="submit" title="submit" />
+              </div>
+              <div className={style.linkButton}>
+                <span onClick={() => navigate(PATH_AUTH.REGISTRATION)}>Registration</span>
+              </div>
             </form>
-            <Button onClick={() => navigate(PATH_AUTH.REGISTRATION)} title="To registration" />
           </div>
+
           <div className={style.img}>
             <img src={Logo} />
           </div>
