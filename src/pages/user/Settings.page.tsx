3837cfd9c4ea1ce5b5ae4df444a0af9af5b2d219
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-10-23 12:31:53 +0300

    add: style to settings user page

--- src/pages/user/Settings.page.tsx
+++ src/pages/user/Settings.page.tsx
@@ -7,27 +7,34 @@ import { useNavigate } from 'react-router-dom';
 import { PATH_HOME } from '../../routes/paths';
 import { sessionActions } from '../../redux/actions/sessionActions';
 import { userSession } from '../../auth/userSession';
+import style from './settings.page.module.scss';
+import InputText from '../../components/InputText.component';
+import { useInputText } from '../../hooks/useInputText.hook';
+import Button from '../../components/Button.component';
 
 export default function UserSettingsPage() {
   const navigate = useNavigate();
   const store = useSelector((state: IStore) => state.user);
 
-  const [newName, setNewName] = useState<string>('');
-  const [oldPass, setOldPass] = useState<string>('');
-  const [newPass, setNewPass] = useState<string>('');
-
-  const [passwordToDelete, setPasswordToDelete] = useState<string>('');
+  const inputName = useInputText();
+  const inputOldPassword = useInputText();
+  const inputNewPassword = useInputText();
+  const inputPasswordToDelete = useInputText();
 
   const updateName = async () => {
-    if (newName === '') return;
-    const editUserName = await userService.editName({ name: newName });
+    if (inputName.value === '') return;
+    const editUserName = await userService.editName({ name: inputName.value });
     if (!editUserName) return;
-    userActions.editName(newName);
+    userActions.editName(inputName.value);
   };
 
   const updatePassword = async () => {
-    if (oldPass === '' || newPass === '') return;
-    const editUserPassword = await userService.editPassword({ password: oldPass, newPassword: newPass });
+    if (inputOldPassword.value === '' || inputNewPassword.value === '') return;
+    if (inputOldPassword.value !== inputNewPassword.value) return;
+    const editUserPassword = await userService.editPassword({
+      password: inputOldPassword.value,
+      newPassword: inputNewPassword.value,
+    });
     if (!editUserPassword) return;
     userActions.logout();
     sessionActions.close();
@@ -35,72 +42,45 @@ export default function UserSettingsPage() {
   };
 
   const deleteUser = async () => {
-    if (passwordToDelete === '') return;
-    await userService.delete({ password: passwordToDelete });
+    if (inputPasswordToDelete.value === '') return;
+    await userService.delete({ password: inputPasswordToDelete.value });
+  };
+
+  const update = () => {
+    updateName();
+    updatePassword();
   };
 
   return (
     <>
-      <div>
-        <span>User name: {store.name}</span>
+      <h2 className={style.name}>User name: {store.name}</h2>
+
+      <div className={style.blockContainer}>
+        <h3>Edit user name</h3>
+        <div className={style.inputContainer}>
+          <InputText title="Edit name" name="name" inputHook={inputName} />
+        </div>
       </div>
 
-      <div>
-        <span>Edit name</span>
-        <input
-          name="user_name"
-          type="text"
-          placeholder="name"
-          onChange={(e) => setNewName(e.target.value)}
-          value={newName}
-        />
-      </div>
-      <div>
-        <br />
-        <button onClick={updateName}>Edit name</button>
-        <br />
-        <br />
+      <div className={style.blockContainer}>
+        <h3>Edit user password</h3>
+        <div className={style.inputContainer}>
+          <InputText title="Old password" name="old_password" inputHook={inputOldPassword} />
+          <InputText title="New password" name="new_password" inputHook={inputNewPassword} />
+        </div>
       </div>
 
-      <div>
-        <span>Edit password old</span>
-        <input
-          name="user_password_old"
-          type="text"
-          placeholder="password old"
-          onChange={(e) => setOldPass(e.target.value)}
-          value={oldPass}
-        />
+      <div className={style.blockContainer}>
+        <h3>Password to confirm delete user account</h3>
+        <div className={style.inputContainer}>
+          <InputText title="Password" name="passwordToDelete" inputHook={inputPasswordToDelete} />
+        </div>
       </div>
 
-      <div>
-        <span>Edit password new</span>
-        <input
-          name="user_password_new"
-          type="text"
-          placeholder="password new"
-          onChange={(e) => setNewPass(e.target.value)}
-          value={newPass}
-        />
-      </div>
-
-      <div>
-        <br />
-        <span>Input password to delete account</span>
-        <input
-          name="delete_account"
-          type="text"
-          placeholder="password to delete"
-          onChange={(e) => setPasswordToDelete(e.target.value)}
-          value={passwordToDelete}
-        />
-      </div>
-
-      <div>
-        <br />
-        <button onClick={updatePassword}>Edit pass</button>
-        <button onClick={() => navigate(PATH_HOME.HOME)}>To home</button>
-        <button onClick={deleteUser}>Delete account</button>
+      <div className={style.buttonContainer}>
+        <Button title="Update" onClick={update} />
+        <Button title="Delete account" onClick={deleteUser} />
+        <Button title="Close" onClick={() => navigate(PATH_HOME.HOME)} />
       </div>
     </>
   );
